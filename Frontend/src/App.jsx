28ce341e2a6f11d17d1e089903a5aab8634aea74
Jenisha <regmijenisha000@gmail.com>
2026-01-07 18:28:37 +0545

    changes made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,8 @@ import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import NavBar from "./components/Navbar.jsx";
 import Faculty from "./pages/Faculty.jsx";
-import CoursePage from "./components/CoursePage.jsx";
+import Courses from "./pages/Courses.jsx";
+
 
 
 const App = () => {
@@ -17,7 +18,7 @@ const App = () => {
         <Route path = "/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/faculty" element={<Faculty/>} />
-        <Route path="/courses" element={<CoursePage/>} />
+        <Route path="/courses" element={<Courses/>} />
       </Routes>
     </BrowserRouter>
   );
